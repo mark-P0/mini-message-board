6866c2feba8b219a184209e60e1eaaca840e58ca
Mark P <mj.poso18@gmail.com>
2023-10-11 20:21:02 +0800

    Add reloader component in index view

--- routes/index.mjs
+++ routes/index.mjs
@@ -1,8 +1,10 @@
 import express from "express";
 
+const isDev = process.env["NODE_ENV"] === "DEVELOPMENT";
+
 export const IndexRouter = express.Router();
 
 /** GET home page. */
 IndexRouter.get("/", (req, res, next) => {
-  res.render("index", { title: "Express" });
+  res.render("index", { isDev, title: "Express" });
 });
