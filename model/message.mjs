9d244bc5ce076b51dd8f22112b6e187dcdf47826
Mark P <mj.poso18@gmail.com>
2023-10-11 23:25:26 +0800

    Add sample messages to index route

---
+++ model/message.mjs
@@ -0,0 +1,24 @@
+/**
+ * @typedef {Object} Message
+ * @property {string} text
+ * @property {string} user
+ * @property {Date} added
+ */
+
+/** @type {Message[]} */
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+export function retrieveMessages() {
+  return [...messages];
+}
--- routes/index.mjs
+++ routes/index.mjs
@@ -1,10 +1,26 @@
 import express from "express";
+import { retrieveMessages } from "../model/message.mjs";
 
 const isDev = process.env["NODE_ENV"] === "DEVELOPMENT";
+
+function formatDate(/** @type {Date} */ date) {
+  return new Intl.DateTimeFormat("en-us", {
+    timeStyle: "short",
+    dateStyle: "short",
+    hour12: false,
+  }).format(date);
+}
 
 export const IndexRouter = express.Router();
 
 /** GET home page. */
 IndexRouter.get("/", (req, res, next) => {
-  res.render("index", { isDev });
+  const messages = retrieveMessages()
+    .reverse()
+    .map((message) => ({
+      ...message,
+      added: formatDate(message.added),
+    }));
+
+  res.render("index", { isDev, messages });
 });
