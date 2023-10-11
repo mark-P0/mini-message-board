8d4f0313d577c300e7eb5d3c4cb19dfe85c10e7c
Mark P <mj.poso18@gmail.com>
2023-10-11 23:15:41 +0800

    Use Tailwind CDN

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ["./index.html", "./**/*.{js,ejs}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = config;
